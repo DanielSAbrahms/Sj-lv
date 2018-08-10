d81ec4a8ae4988459053b3b05c5363069863a982
Work Computer <ws-dabrahms@gonzaga.edu>
2018-08-10 12:38:35 -0700

    Changed Styling and Added Scrollable Sandbox Support

--- scripts/_menuObject.js
+++ scripts/_menuObject.js
@@ -1,10 +1,29 @@
+var origin_x, origin_y;
+
 console.log("menuObject.js Loaded");
 module.exports = function menuObject (type = "noType", name = "noName", active = false) {
   this.type = type;
   this.name = name;
   this.active = active;
+  this.dropHandler = dropHandler;
+  this.doubleClickHandler = doubleClickHandler;
+  this.setOrigin = setOrigin;
+  this.getOrigin = getOrigin;
 }
 
 function dropHandler() {
     //TODO: If drop item exists in the sandbox, then spawn action field
+}
+
+function doubleClickHandler() {
+    console.log("Double Clicked");
+}
+
+function setOrigin(x, y) {
+    origin_x = x;
+    origin_y = y;
+}
+
+function getOrigin() {
+    return [origin_x, origin_y];
 }
--- scripts/menu.js
+++ scripts/menu.js
@@ -3,22 +3,23 @@ $(function() {
 
   var menuObject = require('./_menuObject.js');
   var leftMenu = new menuObject("event", "onClick", false),
+  leftMenu1 = new menuObject("event", "onHover", false),
   rightMenu = new menuObject("action", "doPrint", false);
 
   $("#leftMenuContainer").on('load', loadMenuItems(($("#leftMenuContainer")[0])));
 
   function loadMenuItems(item) {
-    var menuObjects = [leftMenu];
+    var menuObjects = [leftMenu, leftMenu1];
     var str = '<ul>';
     menuObjects.forEach(function(object) {
-      str += '<div class="slot"><li id="' + object.name + '" class="ui-widget-content ui-draggable">' + object.name + '</li></div>';
+      str += '<div class="slot"><li id="' + object.name + '" class="ui-widget-content ui-draggable">'
+      + object.name + '</li></div><div class="row">&nbsp;</div>';
   });
     str += '</ul>';
     item.innerHTML = str;
   }
 
-  var item = $("#onClick").get(0);
-  $(item).draggable({
+  $("#leftMenuContainer li").draggable({
       containment : "#main_container",
       snap: ".slot",
       snapMode: "inner",
@@ -26,7 +27,11 @@ $(function() {
       revert: "invalid"
   });
 
+  $("#leftMenuContainer li").on('dblclick', leftMenu.doubleClickHandler);
+
   $(".slot").droppable( {
       activeClass: "ui-state-highlight"
   });
+
+  $('#sandbox').scrollTop($('#slot_1_container').position().top);
 })
